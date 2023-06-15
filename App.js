4932437232e247352fddaeed7917d021dad6e9f8
Ethan Braunstein <braunstein.ethan@gmail.com>
2023-06-14 23:55:53 -0400

    Added screen navigation

--- App.js
+++ App.js
@@ -2,29 +2,41 @@ import { StatusBar } from "expo-status-bar";
 import * as NavigationBar from "expo-navigation-bar";
 import { Platform } from "react-native";
 import { Provider } from "react-redux";
-import FullscreenAreaView from "./src/components/utils/FullscreenAreaView";
-import TestComponent from "./src/components/utils/TestComponent";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./src/screens/LoginScreen";
 import { store } from "./src/store/storeConfig";
 
-export default function App()  {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
     const initNavigationBar = async () => {
         await NavigationBar.setVisibilityAsync("hidden");
         await NavigationBar.setBehaviorAsync("overlay-swipe");
+        NavigationBar.addVisibilityListener(() => {
+			NavigationBar.setVisibilityAsync("hidden");
+		});
     }
 
     if (Platform.OS === "android") {
         initNavigationBar();
-		NavigationBar.addVisibilityListener(() => {
-			NavigationBar.setVisibilityAsync("hidden");
-		});
     }
     
 	return (
 		<Provider store={store}>
-			<FullscreenAreaView>
-				<StatusBar style="auto" />
-				<TestComponent />
-			</FullscreenAreaView>
+			<NavigationContainer>
+				<Stack.Navigator>
+                    <Stack.Screen
+                        name="Login"
+                        component={LoginScreen}
+                        options={{
+                            title: "Login to Urbauto"
+                        }}
+                    />
+                </Stack.Navigator>
+			</NavigationContainer>
 		</Provider>
 	);
 }
+
+export default App;
