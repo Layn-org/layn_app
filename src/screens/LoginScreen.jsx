407114d84040b2f7db16d39cf5e123dc9ba17a1b
Ethan Braunstein <braunstein.ethan@gmail.com>
2023-06-15 20:49:18 -0400

    Navigate back to Login avec SignUp

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,10 +1,10 @@
 import { Text } from "react-native";
 import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 import TextBox from "../components/input/TextBox";
 import OnOffSwitch from "../components/input/OnOffSwitch";
 import ConfirmButton from "../components/input/ConfirmButton";
-import { setEmail, setPassword, setRememberMe, setSessionInfo } from "../store/loginSlice";
-import { useNavigation } from "@react-navigation/native";
+import { setEmail, setPassword, setRememberMe } from "../store/loginSlice";
 
 const LoginScreen = () => {
     const navigation = useNavigation();
@@ -12,7 +12,6 @@ const LoginScreen = () => {
 	const email = useSelector((state) => state.login.email);
 	const password = useSelector((state) => state.login.password);
 	const rememberMe = useSelector((state) => state.login.rememberMe);
-	const sessionInfo = useSelector((state) => state.login.sessionInfo);
 
     const handleLogin = () => {
         console.log("Login info:", email, password, rememberMe)
--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,20 +1,23 @@
 import { Text } from "react-native";
 import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 import TextBox from "../components/input/TextBox";
 import OnOffSwitch from "../components/input/OnOffSwitch";
 import ConfirmButton from "../components/input/ConfirmButton";
-import { setEmail, setPassword, setRememberMe, setSessionInfo } from "../store/loginSlice";
+import { setEmail, setPassword, setRememberMe } from "../store/loginSlice";
 import { signUp } from "../supabase/auth";
 
 const SignUpScreen = () => {
+    const navigation = useNavigation();
+
 	const email = useSelector((state) => state.login.email);
 	const password = useSelector((state) => state.login.password);
 	const rememberMe = useSelector((state) => state.login.rememberMe);
-	const sessionInfo = useSelector((state) => state.login.sessionInfo);
 
     const handleSignUp = () => {
         console.log("Sign up info:", email, password, rememberMe)
         signUp(email, password)
+		navigation.navigate("Login");
     }
 
 	return (
