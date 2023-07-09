d4582190d2d382477372b5fb4a4f741dac263a35
Ethan Braunstein <braunstein.ethan@gmail.com>
2023-07-09 17:23:36 +0200

    Started MainCard component

--- src/components/interface/MainCard.jsx
+++ src/components/interface/MainCard.jsx
@@ -1,19 +1,35 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import { colors } from "../../constants/styles";
 
 const MainCard = ({title}) => {
     return (
-        <View style={styles.outerCard}>
-            
-        </View>
-    )
+		<View style={styles.outerCard}>
+			<Image
+				style={styles.image}
+				source={require("../../assets/temp/toyota-corolla-my19-index-_281_29.png")}
+			/>
+			<Text style={styles.title}>{title}</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
     outerCard: {
-        flex: 1,
         backgroundColor: colors.secondaryAccent,
-        borderRadius: 5,
+        borderRadius: 25,
+        height: 200,
+        marginHorizontal: 10,
+        marginTop: 10,
+    },
+    title: {
+        color: colors.white,
+        fontSize: 30,
+        marginLeft: 5,
+    },
+    image: {
+        height: 80,
+        width: 120,
+        margin: 5,
     }
 })
 
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,13 +1,20 @@
-import { Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import MainCard from "../components/interface/MainCard";
+import { colors } from "../constants/styles";
 
 const HomeScreen = () => {
 	return (
-		<>
-			<Text>You are on the homesceen!</Text>
+		<View style={styles.main}>
 			<MainCard title="Toyota Corolla"/>
-		</>
+		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        backgroundColor: colors.secondary
+    }
+})
 
 export default HomeScreen;
